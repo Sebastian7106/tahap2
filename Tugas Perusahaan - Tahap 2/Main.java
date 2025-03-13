3cfcd169f6b249ef23eadbb11621328ec03797a5
Sebastian Noel <sebastiannoel@student.ub.ac.id>
2025-03-13 21:30:43 +0700

    Add files via upload

---
+++ Tugas Perusahaan - Tahap 2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Perusahaan p1 = new Perusahaan("PT. Sebastian");
+    Karyawan k1 = new Karyawan("Noel", 3000000, p1);
+    k1.info();
+    Programmer pro1 = new Programmer("Kristian", 8000000, 2000000, p1);
+    pro1.info();
+    Manager man1 = new Manager("Christopher", 15000000, 5000000, p1);
+    man1.info();
+  }
+}
